d776bebef7ec6dade934e0920c1e5433baaac2ea
eumji <jihye97@ajou.ac.kr>
2017-11-09 17:19:21 +0900

    change

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,28 @@
 #include "signal_handlers.h"
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include<signal.h>
+#include <stdio.h>
 
 void catch_sigint(int signalNo)
 {
-  // TODO: File this!
+  // char *s;
+  //s=strsignal(signalNo);
+  //signal(SIGINT, SIG_IGN);
+ // char buf[4];
+ // fgets(buf, sizeof(char)*4, stdin);
+  signal(signalNo, SIG_IGN);
+ return;
 }
 
 void catch_sigtstp(int signalNo)
 {
-  // TODO: File this!
+  //char *s;
+  //s=strsignal(signalNo);
+  //signal(SIGINT, SIG_IGN);
+  //char buf[4];
+  //fgets(buf, sizeof(char)*4, stdin);
+  signal(signalNo, SIG_IGN);
+  return;
 }
